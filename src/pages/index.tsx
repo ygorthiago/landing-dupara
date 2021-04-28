703f58b9bde7ca845f889359ca03e5a734fa758c
ygorthiago <ygorthiagoff@outlook.com>
2021-04-28 13:52:04 -0300

    feat: add about us to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { ContactForm } from "../components/ContactForm";
+import { AboutUs } from "../components/Home/AboutUs";
+import { Contact } from "../components/Home/Contact";
 import { FirstSection } from "../components/Home/FirstSection";
 
 import s from "../styles/home.module.scss";
@@ -7,7 +8,8 @@ export default function Home() {
   return (
     <div className={s.container}>
       <FirstSection />
-      <ContactForm />
+      <AboutUs />
+      <Contact />      
     </div>
   );
 }
