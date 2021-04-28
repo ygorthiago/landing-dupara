d5a0b6763ad3e80e395c5ee0f8bc4e06e5f88fcc
ygorthiago <ygorthiagoff@outlook.com>
2021-04-28 11:54:21 -0300

    feat: desktop header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import Sidebar from './HeaderMobile';
+import s from "./styles.module.scss";
+
+export function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggle() {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <header className={s.headerContainer}>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+
+      <div className={isOpen ? s.mobileIconOpened : s.mobileIcon} onClick={toggle}>
+        <input type="checkbox" checked={isOpen} readOnly />
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={s.menuItems}>
+        <li>
+          <a href="/">Início</a>
+        </li>
+        <li>
+          <a href="#about-us">Sobre nós</a>
+        </li>
+        <li>
+          <a href="#product">Nosso produto</a>
+        </li>
+        <li>
+          <a href="#contact">Contato</a>
+        </li>
+      </ul>
+    </header>
+  )
+}
