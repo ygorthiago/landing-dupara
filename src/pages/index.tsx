659928802371fac8e4348511961c8e036e5e7725
ygorthiago <ygorthiagoff@outlook.com>
2021-04-28 11:07:10 -0300

    feat: add contact form to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,11 @@
 import { useState } from "react";
+import { ContactForm } from "../components/ContactForm";
 import { api } from "../services/api";
 
 export default function Home() {
-  const [subject, setSubject] = useState('');
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-
-  async function handleContactSubmit(data) {
-    data.preventDefault();
-
-    try {
-      await api.post('/sendmail', {
-        userName: name,
-        userEmail: email,
-        text: message,
-        subject
-      });
-    } catch (error) {
-      console.log(error);
-    }    
-  }
-
   return (
-   <form action="" onSubmit={handleContactSubmit}>
-     <input type="text" name="subject" placeholder="subject" onChange={e => setSubject(e.target.value)} />
-     <input type="text" name="name" placeholder="name" onChange={e => setName(e.target.value)} />
-     <input type="text" name="email" placeholder="email" onChange={e => setEmail(e.target.value)} />
-     <input type="text" name="message" placeholder="message" onChange={e => setMessage(e.target.value)} />
-
-    <button type="submit">Submit</button>
-
-   </form>
-  )
+    <div>
+      <ContactForm />
+    </div>
+  );
 }
