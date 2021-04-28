8a1107715d122a9c6d183454aa7a429346cac57f
ygorthiago <ygorthiagoff@outlook.com>
2021-04-28 11:06:54 -0300

    feat: contact form

---
+++ src/components/ContactForm/index.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { api } from '../../services/api';
+
+import s from "./styles.module.scss";
+
+export function ContactForm() {
+  const [subject, setSubject] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  async function handleContactSubmit(data) {
+    data.preventDefault();
+
+    try {
+      await api.post('/sendmail', {
+        userName: name,
+        userEmail: email,
+        text: message,
+        subject
+      });
+    } catch (error) {
+      console.log(error);
+    }    
+  }
+
+  return (
+   <form action="" onSubmit={handleContactSubmit} className={s.formContainer}>
+     <input type="text" name="subject" placeholder="subject" onChange={e => setSubject(e.target.value)} />
+     <input type="text" name="name" placeholder="name" onChange={e => setName(e.target.value)} />
+     <input type="text" name="email" placeholder="email" onChange={e => setEmail(e.target.value)} />
+     <textarea name="message" placeholder="message" onChange={e => setMessage(e.target.value)} />
+
+    <button type="submit">Enviar</button>
+
+   </form>
+  )
+}
