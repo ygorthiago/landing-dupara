d69465b51b2be74c5fbdc70fce5ee6ccff29becc
ygorthiago <ygorthiagoff@outlook.com>
2021-04-29 15:00:49 -0300

    feat: social media section

--- src/components/Home/Contact/index.tsx
+++ src/components/Home/Contact/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { api } from '../../../services/api';
+import { FaInstagram, FaFacebook, FaYoutube } from 'react-icons/fa';
 
 import s from "./styles.module.scss";
 
@@ -22,14 +23,14 @@ export function Contact() {
           userEmail: email,
           text: message,
         });
-  
+
         alert("E-mail enviado com sucesso!");
         setLoading(false);
       } catch (error) {
         setLoading(false);
         console.log(error);
         setErrorMessage("Algum erro ocorreu. Tente novamente.");
-      }    
+      }
     } else {
       setLoading(false);
       setErrorMessage('Todos os campos devem ser preenchidos!');
@@ -43,9 +44,9 @@ export function Contact() {
       </div>
 
       <p className={s.contactInfo}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus augue tortor, 
-        vel commodo massa luctus feugiat. Pellentesque sed orci nulla. Nulla facilisi. Cras 
-        viverra mauris orci, nec porttitor dolor finibus lobortis. Nulla facilisi. Phasellus 
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus augue tortor,
+        vel commodo massa luctus feugiat. Pellentesque sed orci nulla. Nulla facilisi. Cras
+        viverra mauris orci, nec porttitor dolor finibus lobortis. Nulla facilisi. Phasellus
         ut enim sit amet nulla posuere tincidunt. Aenean eget velit justo. Pellentesque vitae.
       </p>
 
@@ -53,13 +54,44 @@ export function Contact() {
         <input type="text" name="name" placeholder="Nome" onChange={e => setName(e.target.value)} />
         <input type="email" name="email" value={email} placeholder="E-mail" onChange={e => setEmail(e.target.value)} />
         <textarea name="message" placeholder="Mensagem" onChange={e => setMessage(e.target.value)} />
-        
-        { errorMessage && <span>{errorMessage}</span> }
+
+        {errorMessage && <span>{errorMessage}</span>}
 
         <button type="submit" disabled={loading}>
-          { loading ? <div className={s.loader}></div> : "Enviar" }
+          {loading ? <div className={s.loader}></div> : "Enviar"}
         </button>
       </form>
+
+      <div className={s.socialSection}>
+        <a
+          href="//www.instagram.com"
+          target="_blank"
+          aria-label="Instagram"
+        >
+          <div>
+            <FaInstagram />
+          </div>
+        </a>
+        <a
+          href="//www.facebook.com"
+          target="_blank"
+          aria-label="Facebook"
+        >
+          <div>
+            <FaFacebook />
+          </div>
+        </a>
+        <a
+          href="//www.youtube.com"
+          target="_blank"
+          aria-label="Youtube"
+        >
+          <div>
+            <FaYoutube />
+          </div>
+        </a>
+
+      </div>
     </section>
   )
 }
