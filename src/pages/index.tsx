8e01ae84a7b8286175401a1661b20f0d5f86eb98
siteduparadev <86215156+siteduparadev@users.noreply.github.com>
2022-02-18 10:30:59 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,15 @@ export default function Home() {
       <Head>
         <title>Gordura de Palma Du Pará</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-PNB575L6FB"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-PNB575L6FB');
+</script>
       </Head>
       <Header />
       <FirstSection />
